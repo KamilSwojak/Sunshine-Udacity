181f62f83fe51b90cba2ec6270f366c2a60a724c
kamil.swojak <kamil.swojak@gmail.com>
2016-06-11 18:01:20 +0200

    Add icon to LocationEditTextPreference

--- app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
+++ app/src/main/java/com/example/android/sunshine/app/LocationEditTextPreference.java
@@ -24,8 +24,15 @@ import android.preference.EditTextPreference;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class LocationEditTextPreference extends EditTextPreference {
     static final private int DEFAULT_MINIMUM_LOCATION_LENGTH = 2;
@@ -42,8 +49,27 @@ public class LocationEditTextPreference extends EditTextPreference {
         } finally {
             a.recycle();
         }
+
+        if (checkPlayServices()) {
+            setWidgetLayoutResource(R.layout.pref_current_location);
+        }
     }
 
+    @Override
+    protected View onCreateView(ViewGroup parent) {
+        View view = super.onCreateView(parent);
+        View currentLocation = view.findViewById(R.id.current_location);
+
+        if (currentLocation != null) {
+            currentLocation.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(), "Success", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        return view;
+    }
 
     @Override
     protected void showDialog(Bundle state) {
@@ -51,16 +77,12 @@ public class LocationEditTextPreference extends EditTextPreference {
 
         EditText et = getEditText();
         et.addTextChangedListener(new TextWatcher() {
-
-
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
             }
 
             @Override
@@ -80,5 +102,14 @@ public class LocationEditTextPreference extends EditTextPreference {
                 }
             }
         });
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK.
+     */
+    private boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(getContext());
+        return resultCode == ConnectionResult.SUCCESS;
     }
 }
